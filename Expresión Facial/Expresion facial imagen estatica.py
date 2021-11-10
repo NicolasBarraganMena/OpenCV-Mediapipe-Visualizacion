bec25aca78137e453c38cb6d9a716ef96d8cf07d
Nicolas Barragan Mena <nicolas.barragan@upb.edu.co>
2021-11-10 15:33:19 -0500

    Importar mediapipe y opencv

---
+++ Expresión Facial/Expresion facial imagen estatica.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 10 15:08:37 2021
+
+@author: Nicolas Barragan
+"""
+import cv2
+import mediapipe as mp
+
+mp_face_mesh = mp.solutions.face_mesh
+mp_drawing = mp.solutions.drawing_utils
+
+with mp_face_mesh.FaceMesh(
+        static_image_mode = True,
+        max_num_faces = 1,
+        min_detection_confidence = 0.5) as face_mesh:
+    
+    imagen = cv2.imshow("CR7");
