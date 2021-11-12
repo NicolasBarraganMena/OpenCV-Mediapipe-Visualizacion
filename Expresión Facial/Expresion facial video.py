6c5e970275265d2682bdc8a48b73c6dc0023812f
Nicolas Barragan Mena <nicolas.barragan@upb.edu.co>
2021-11-12 12:00:05 -0500

    Detección sorpresa y enojo

--- Expresión Facial/Expresion facial video.py
+++ Expresión Facial/Expresion facial video.py
@@ -57,8 +57,8 @@ with mp_face_mesh.FaceMesh(
                 p2 = [int(face_landmarks.landmark[ojo_izq].x * width), 
                       int(face_landmarks.landmark[ojo_izq].y * height)]
                 cv2.circle(frame, p2, 2, (255,0,255), 2)
-                d1 = str(int(math.dist(p1, p2)))
-                cv2.putText(frame, d1, (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,255), 2)
+                d1 = int(math.dist(p1, p2))
+                cv2.putText(frame, str(d1), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,255), 2)
                 '''Ceja derecha - ojo derecho'''
                 p3 = [int(face_landmarks.landmark[ceja_der].x * width), 
                       int(face_landmarks.landmark[ceja_der].y * height)]
@@ -66,8 +66,8 @@ with mp_face_mesh.FaceMesh(
                 p4 = [int(face_landmarks.landmark[ojo_der].x * width), 
                       int(face_landmarks.landmark[ojo_der].y * height)]
                 cv2.circle(frame, p4, 2, (0,255,255), 2)
-                d2 = str(int(math.dist(p3, p4)))
-                cv2.putText(frame, d2, (50,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,255), 2)
+                d2 = int(math.dist(p3, p4))
+                cv2.putText(frame, str(d2), (50,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,255), 2)
                 '''Extremos boca'''
                 p5 = [int(face_landmarks.landmark[extremo_der].x * width), 
                       int(face_landmarks.landmark[extremo_der].y * height)]
@@ -75,8 +75,8 @@ with mp_face_mesh.FaceMesh(
                 p6 = [int(face_landmarks.landmark[extremo_izq].x * width), 
                       int(face_landmarks.landmark[extremo_izq].y * height)]
                 cv2.circle(frame, p6, 2, (0,0,255), 2)
-                d3 = str(int(math.dist(p5, p6)))
-                cv2.putText(frame, d3, (50,150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+                d3 = int(math.dist(p5, p6))
+                cv2.putText(frame, str(d3), (50,150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
                 '''Apertura boca'''
                 p7 = [int(face_landmarks.landmark[labio_inf].x * width), 
                       int(face_landmarks.landmark[labio_inf].y * height)]
@@ -84,9 +84,16 @@ with mp_face_mesh.FaceMesh(
                 p8 = [int(face_landmarks.landmark[labio_sup].x * width), 
                       int(face_landmarks.landmark[labio_sup].y * height)]
                 cv2.circle(frame, p8, 2, (0,255,0), 2)
-                d4 = str(int(math.dist(p7, p8)))
-                cv2.putText(frame, d4, (50,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
+                d4 = int(math.dist(p7, p8))
+                cv2.putText(frame, str(d4), (50,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
                 
+                '''Detectar expresión'''
+                '''enojado'''
+                if  (d1<20 and d1>10 and d2<20 and d2>10 and d3<90 and d3>54 and d4<2):
+                    cv2.putText(frame, 'Enojado', (100,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
+                '''sorpresa'''
+                if  (d1>30 and d2>30 and d3<78 and d3>46 and d4>20):
+                    cv2.putText(frame, 'Sorpresa', (100,250), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
         
         cv2.imshow("Video", frame)
         k = cv2.waitKey(1) & 0xFF
