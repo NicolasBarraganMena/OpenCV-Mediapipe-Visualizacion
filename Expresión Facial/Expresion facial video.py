1736a5d1e9430a8e9b712e49c1509b6e77c98166
Nicolas Barragan Mena <nicolas.barragan@upb.edu.co>
2021-11-11 19:07:26 -0500

    Calcular distancias entre puntos clave vídeo

--- Expresión Facial/Expresion facial video.py
+++ Expresión Facial/Expresion facial video.py
@@ -7,10 +7,11 @@ Created on Thu Nov 05 14:19:54 2021
 
 import cv2
 import mediapipe as mp
+import math
 
 mp_face_mesh = mp.solutions.face_mesh
 mp_drawing = mp.solutions.drawing_utils
-
+'''
 ceja_izq = [70, 63, 105, 66, 107]
 ceja_der = [336, 296, 334, 293, 300]
 labio_sup = [184, 74, 73, 72, 11, 302, 303, 304, 408]
@@ -19,6 +20,15 @@ parpados_sup = [246, 161, 160, 159, 158, 157, 173,
                 398, 384, 385, 386, 387, 388, 466]
 parpados_inf = [7, 163, 144, 145, 153, 154, 155,
                 382, 381, 380, 374, 373, 390, 249]
+'''
+ceja_izq = 65
+ojo_izq = 158
+ceja_der = 295
+ojo_der = 385
+extremo_der = 308
+extremo_izq = 78
+labio_inf = 14
+labio_sup = 13
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
@@ -40,36 +50,43 @@ with mp_face_mesh.FaceMesh(
         if results.multi_face_landmarks is not None:
             for face_landmarks in results.multi_face_landmarks:
                 
-                '''Ceja izquierda'''
-            for index in ceja_izq:
-                x = int(face_landmarks.landmark[index].x * width)
-                y = int(face_landmarks.landmark[index].y * height)
-                cv2.circle(frame, (x,y), 2, (255,0,255), 2)
-            '''Ceja derecha'''
-            for index in ceja_der:
-                x = int(face_landmarks.landmark[index].x * width)
-                y = int(face_landmarks.landmark[index].y * height)
-                cv2.circle(frame, (x,y), 2, (255,0,255), 2)
-            '''Labio superior'''
-            for index in labio_sup:
-                x = int(face_landmarks.landmark[index].x * width)
-                y = int(face_landmarks.landmark[index].y * height)
-                cv2.circle(frame, (x,y), 2, (0,255,255), 2)
-            '''Labio inferior'''
-            for index in labio_inf:
-                x = int(face_landmarks.landmark[index].x * width)
-                y = int(face_landmarks.landmark[index].y * height)
-                cv2.circle(frame, (x,y), 2, (0,0,255), 2)
-            '''Parpado superior'''
-            for index in parpados_sup:
-                x = int(face_landmarks.landmark[index].x * width)
-                y = int(face_landmarks.landmark[index].y * height)
-                cv2.circle(frame, (x,y), 2, (255,0,0), 2)
-            '''Parpado inferior'''
-            for index in parpados_inf:
-                x = int(face_landmarks.landmark[index].x * width)
-                y = int(face_landmarks.landmark[index].y * height)
-                cv2.circle(frame, (x,y), 2, (0,255,0), 2)
+                '''Ceja izquierda - ojo izquierdo'''
+                p1 = [int(face_landmarks.landmark[ceja_izq].x * width), 
+                      int(face_landmarks.landmark[ceja_izq].y * height)]
+                cv2.circle(frame, p1, 2, (255,0,255), 2)
+                p2 = [int(face_landmarks.landmark[ojo_izq].x * width), 
+                      int(face_landmarks.landmark[ojo_izq].y * height)]
+                cv2.circle(frame, p2, 2, (255,0,255), 2)
+                d1 = str(int(math.dist(p1, p2)))
+                cv2.putText(frame, d1, (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,255), 2)
+                '''Ceja derecha - ojo derecho'''
+                p3 = [int(face_landmarks.landmark[ceja_der].x * width), 
+                      int(face_landmarks.landmark[ceja_der].y * height)]
+                cv2.circle(frame, p3, 2, (0,255,255), 2)
+                p4 = [int(face_landmarks.landmark[ojo_der].x * width), 
+                      int(face_landmarks.landmark[ojo_der].y * height)]
+                cv2.circle(frame, p4, 2, (0,255,255), 2)
+                d2 = str(int(math.dist(p3, p4)))
+                cv2.putText(frame, d2, (50,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,255), 2)
+                '''Extremos boca'''
+                p5 = [int(face_landmarks.landmark[extremo_der].x * width), 
+                      int(face_landmarks.landmark[extremo_der].y * height)]
+                cv2.circle(frame, p5, 2, (0,0,255), 2)
+                p6 = [int(face_landmarks.landmark[extremo_izq].x * width), 
+                      int(face_landmarks.landmark[extremo_izq].y * height)]
+                cv2.circle(frame, p6, 2, (0,0,255), 2)
+                d3 = str(int(math.dist(p5, p6)))
+                cv2.putText(frame, d3, (50,150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+                '''Apertura boca'''
+                p7 = [int(face_landmarks.landmark[labio_inf].x * width), 
+                      int(face_landmarks.landmark[labio_inf].y * height)]
+                cv2.circle(frame, p7, 2, (255,0,0), 2)
+                p8 = [int(face_landmarks.landmark[labio_sup].x * width), 
+                      int(face_landmarks.landmark[labio_sup].y * height)]
+                cv2.circle(frame, p8, 2, (0,255,0), 2)
+                d4 = str(int(math.dist(p7, p8)))
+                cv2.putText(frame, d4, (50,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
+                
         
         cv2.imshow("Video", frame)
         k = cv2.waitKey(1) & 0xFF
